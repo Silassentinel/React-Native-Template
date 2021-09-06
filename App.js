05d0a08f9ec1f450a8f0ed1df4850e367a155273
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2021-09-06 05:16:49 +0200

    Added store to template + update on some packages

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from "./src/Stylez/StyleSheet";
 
 export default function App() {
   return (
@@ -11,11 +12,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
